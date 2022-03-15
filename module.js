5d9dcc42b891a34289c8d5e8b4fd59cec28d153b
jacob jemal <jacobjemal7@gmail.com>
2022-03-15 15:45:40 -0400

    added a button

--- module.js
+++ module.js
@@ -1,7 +1,10 @@
-let myName = prompt('enter you name')
-let month = ('what month were you born?')
-let food = ('what is your favorite food')
+function quiz () {
+    let myName = prompt('enter you name')
+    let month = prompt('what month were you born?')
+    let food = prompt('what is your favorite food')
+    console.log('my name is', myName)
+    console.log('i was born in', month)
+    console.log('my favorite food is', food)
+}
 
-console.log('my name is', myName)
-console.log('i was born in', month)
-console.log('my favorite food is', food)
+quiz()
