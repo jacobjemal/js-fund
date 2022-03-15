45967766d5802e27b061fcc216197345ed580f8e
jacob jemal <jacobjemal7@gmail.com>
2022-03-15 15:31:56 -0400

    added new jf file

--- index.js
+++ index.js
@@ -1,6 +1,2 @@
 console.log('index has loaded')
-let score = 100
-
-let email = 
-let h1 = document.createElement('h1')
-h1.innerText = email 
+// let score = 100
--- module.js
+++ module.js
@@ -1,1 +1,7 @@
-console.log('module has loaded')
+let myName = prompt('enter you name')
+let month = ('what month were you born?')
+let food = ('what is your favorite food')
+
+console.log('my name is', myName)
+console.log('i was born in', month)
+console.log('my favorite food is', food)
