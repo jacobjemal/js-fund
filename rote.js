6e96639945c71b3ddec0b77253e5de804a6a1b22
jacob jemal <jacobjemal7@gmail.com>
2022-03-16 13:31:42 -0400

    funcs

---
+++ rote.js
@@ -0,0 +1,17 @@
+const greet = () => {
+    console.log('hello world')
+}
+
+greet()
+
+const square = (num) => {
+    return num * num
+}
+
+square(25)
+
+const greetByName = (str) => {
+    console.log('hello', str)
+}
+
+greetByName('jacob')
