20f1db1b4a7d8940a4864614f5814abebd160257
jacob jemal <jacobjemal7@gmail.com>
2022-03-16 12:10:16 -0400

    functions

--- module.js
+++ module.js
@@ -1,4 +1,15 @@
-function quiz () {
+
+
+
+
+
+
+
+
+
+
+
+const quiz = () => {
     let myName = prompt('enter you name')
     let month = prompt('what month were you born?')
     let food = prompt('what is your favorite food')
@@ -7,4 +18,57 @@ function quiz () {
     console.log('my favorite food is', food)
 }
 
-quiz()
+const print = () => {
+    console.log('I love to print text!')
+}
+
+const functionA = (f) => {
+    console.log('FUNCTION A HAS BEEN CALLED')
+    // functionB()
+    f()
+}
+
+const functionB = () => {
+    console.log('FUNCTION B HAS BEEN CALLED')
+}
+
+const functionC = () => {
+    console.log('FUNCTION C HAS BEEN CALLED')
+}
+setInterval(functionC, 1000)
+
+const greet = (person) => {
+    console.log('HELLO', person)
+}
+
+const blender = (string) => {
+    return string.split('')
+}
+
+const square = (num, message) => {
+    console.log('message:', message)
+    return num * num 
+}
+
+
+
+
+
+
+
+
+
+
+
+
+// function notifyB() {
+// name: function() {
+// console.log('this is', this)
+//     }
+// }
+
+// const notifyA = () => {
+//     name: () =>{
+// console.log('this is', this)
+//     }
+// }
