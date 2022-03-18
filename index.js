c988134ab5f2a7156464bd79bf3a95fa5b5f2840
jacob jemal <jacobjemal7@gmail.com>
2022-03-18 10:20:29 -0400

    building array

--- index.js
+++ index.js
@@ -1,34 +1,72 @@
-const multiply = (n1, n2) => {
-    console.log("multiplying", n1, "by", n2)
-    return n1 * n2
+let apt1 = {
+    bedrooms: 2, 
+    sqft: 50, 
+    bathrooms: 3, 
+    windows: 0, 
+    rent: 4500, 
+    unit: '1A',
+    tenants: []
 }
 
-const double = (n) =>{
-    return n * 2
+let apt2 = {
+    bedrooms: 1, 
+    sqft: 500, 
+    bathrooms: 1, 
+    windows: 2, 
+    rent: 1500, 
+    unit: '1A',
+    tenants: []
 }
 
-let array1 = ['a', 'b', 'c', 'd','e']
-let array2 = [1,2,3,4,5,6,7]
-
-const determineLenght = (arr) => {
-    if (arr.lenght > 5) {
-        return 'this array is long'
-    } else {
-        return 'this array is short'
-    }
-}
-const askForString = () => {
-    let str = prompt ('enter a string')
-    return str
+let apt3 = {
+    bedrooms: 2, 
+    sqft: 53, 
+    bathrooms: 3, 
+    windows: 2, 
+    rent: 2400, 
+    unit: '1C',
+    tenants: []
 }
 
-console.log(askForString().split(''))
+let apt4 = {
+    bedrooms: 0, 
+    sqft: 11, 
+    bathrooms: 0, 
+    windows: 0, 
+    rent: 900, 
+    unit: '1D',
+    tenants: []
+}
 
+let tenant1 = {
+    name: 'John Doe', 
+    creditScore: 400,
+    salary: 15000, 
+    age: 30, 
+    pet: 'dog'
+}
 
-const array = ['jay', 'bob', 'jim', 'dave']
+let tenant2 = {
+    name: 'Jane Doe', 
+    creditScore: 800,
+    salary: 22000, 
+    age: 33 
+}
+        
 
-const newArray = array.map((el) => {
-    return el.toUppercase()
-})
-
-console.log(newArray)
+let building = {
+    streetAddress: '74 N 7th Street, Brooklyn NY 11249',
+    laundry: false,
+    allowsPets: false,
+    lease: function(apt, tenant) {
+        if (apt.tenants.length === apt.bedrooms) {
+        return `${apt.unit} is already full!`
+        }
+        if (this.allowsPets === false && tenant.pet){
+            return `${apt.unit} is available, but you must give ${tenant.pet} up for adoption!`
+        }
+        apt.tenants.push(tenant)
+        console.log(tenant.name, 'has rented out', apt.unit)
+    },
+    apartments: [apt1, apt2, apt3]
+}
